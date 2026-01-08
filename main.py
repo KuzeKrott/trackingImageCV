2e4a934cbfee6cc0d435cfea2ac02b20232a7094
KuzeKrott <christina.krot@yandex.ru>
2026-01-08 18:09:21 +0300

    gets image, moves figure, quit with esc

--- main.py
+++ main.py
@@ -1,34 +1,38 @@
 import cv2
 import numpy as np
+import ctypes
 
-mouse_x, mouse_y = -1, -1
+ctypes.windll.user32.ShowCursor(False) # делаем курсор невидимым 
+mouse_x, mouse_y = 0, 0
+figureSize = 50
+video_capture = cv2.VideoCapture(0)
 
 #callback
 def on_mouse(event, x, y, flags, param):
     global mouse_x, mouse_y
     if event == cv2.EVENT_MOUSEMOVE:
         mouse_x, mouse_y = x, y
-        print(f"Mouse moved to: ({mouse_x}, {mouse_y})")
 
-# создаем пустое изображение
 img = np.zeros((600, 800, 3), dtype=np.uint8)
-
-# задаем имя окна
 win_name = "Mouse Tracker"
 cv2.namedWindow(win_name)
 
-# регистрируем callback
-cv2.setMouseCallback(win_name, on_mouse)
+cv2.setMouseCallback(win_name, on_mouse) # функция из OpenCV, передаётся ссылка на ф-цию on_mouse
 
-print("Move mouse inside the window...")
+half = figureSize//2
 while True:
-    display = img.copy()
-    cv2.putText(display, f"X: {mouse_x}, Y: {mouse_y}",
-                (10, 30), cv2.FONT_HERSHEY_SIMPLEX,
-                1, (0, 255, 0), 2)
+    result, display = video_capture.read()  # read frames from the video
+    cv2.rectangle(display, 
+                  (mouse_x - half, mouse_y - half), 
+                  (mouse_x + half, mouse_y + half), 
+                  (255, 0, 0), 
+                  5)
 
     cv2.imshow(win_name, display)
-    if cv2.waitKey(20) & 0xFF == 27:  # выход по ESC
+
+    if cv2.waitKey(20) & 0xFF == 27: 
         break
 
+ctypes.windll.user32.ShowCursor(True)
+video_capture.release()
 cv2.destroyAllWindows()
